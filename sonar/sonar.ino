ae2c038520aa302ea1d00f557a1ed01720d65135
hwjeremy <hugh.jeremy@gmail.com>
2015-06-12 19:07:14 +1000

    ping time, temp, humidity reads, re-org file

--- sonar/sonar.ino
+++ sonar/sonar.ino
@@ -3,15 +3,29 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <EthernetUdp.h>
+#include <NewPing.h>
+#include <DHT.h>
 
-byte mac[] = {};
+//network
+byte mac[] = {}; //set mac!
 IPAddress localIp(192, 168, 1, 101);
 IPAddress targetIp(192, 168, 1, 100); //target minnie
-short port = 1234;
+const short port = 1234;
+EthernetUDP Udp;
+
+//pinger
+const byte pin_trigger = 1;
+const byte pin_echo = 2;
+const short maxDistance = 400; //in centimetres - check tank height!
+NewPing pinger(pin_trigger, pin_echo, maxDistance);
+
+//temp & humidity
+const byte pin_dht = 3;
+const byte dhtType = 22;
+bool dhtValid = false;
+DHT dht(pin_dht, dhtType);
 
 short waitTime = 5000; //in milliseconds
-
-EthernetUDP Udp;
 
 void setup() {
 	Ethernet.begin(mac, localIp); //dns & gateway default to ip with final octet 1
@@ -20,6 +34,23 @@ void setup() {
 
 void loop() {
 	//get sensor data
+		//v = 331.4 + 0.6T m/s, T = temp in degrees celsius
+		//v = 331.4 + 0.6T m/s + 0.0124*H, H = relative humidity %
+	
+	unsigned int pingTime = pinger.ping();
+		//consider sending several pings a-la NewPing.h ping_median
+	
+	float h = dht.readHumidity();
+	float t = dht.readTemperature();
+	
+	if (isnan(h) || isnan(t))
+	{
+		dhtValid = false;
+	}
+	else
+	{
+		dhtValid = true;
+	}
 	
 	//send sensor data
 	Udp.beginPacket(targetIp, port);
