695c42914a2952b00f0e9b830fed6450a7f98c3b
hwjeremy <hugh.jeremy@gmail.com>
2015-06-21 12:54:57 +1000

    update production version to include upgrades in local testing version

--- sonar/sonar.ino
+++ sonar/sonar.ino
@@ -1,18 +1,18 @@
-// sonar
+// sonar - local testing version
 // to control an Arduino Uno based ultrasonic distance sensor
 
 #include <SPI.h>
-#include <Ethernet.h>
+#include <Ethernet.h> //still include for memory footprint simulation
 #include <EthernetUdp.h>
 #include <NewPing.h>
 #include <DHT.h>
 #include <ArduinoJson.h>
 
-//network
-byte mac[] = {0x32, 0x81, 0x7F, 0x25, 0x6a, 0x7d}; //set mac!
-byte sensorId = 1;
-byte sensorType = 1;
-IPAddress localIp(192, 168, 1, 101);
+//network - not functional in test, set for memory footprint
+const byte mac[] = {0x32, 0x81, 0x7F, 0x25, 0x6a, 0x7d}; //set mac!
+const byte sensorId = 1;
+const byte sensorType = 1;
+IPAddress localIpFallback(192, 168, 1, 101);
 IPAddress targetIp(192, 168, 1, 100); //target minnie
 const short port = 1234;
 EthernetUDP Udp;
@@ -20,55 +20,78 @@ EthernetUDP Udp;
 //pinger
 const byte pin_trigger = 7;
 const byte pin_echo = 6;
-const int maxDistance = 400; //in centimetres - check tank height!
+const int maxDistance = 200; //in centimetres - check tank height!
 NewPing pinger(pin_trigger, pin_echo, maxDistance);
 
 //temp & humidity
-const byte pin_dht = 3;
+const byte pin_dht = 2;
 const byte dhtType = 22;
 DHT dht(pin_dht, dhtType);
 
 //watchdog
 const byte pin_watch = 4;
 
-short waitTime = 5000; //in milliseconds
+//indicator led (blink after each transmission)
+const byte pin_led = 13;
+const short ledInterval = 1000; //in milliseconds. Must be lower than transmissionInterval
+bool ledState = LOW;
+
+//transmission interval
+const short transmissionInterval = 5000; //in milliseconds. Must be higher than ledInterval
+unsigned long lastTransmissionTime = 0;
 
 void setup() {
-	Ethernet.begin(mac, localIp); //dns & gateway default to ip with final octet 1
+	//attempt to get an ip address via DHCP
+	if (Ethernet.begin(mac) == 0)
+	{
+		//fall back to default ip
+		Ethernet.begin(mac, localIpFallback) //dns & gateway default to ip with final octet 1
+	}
 	Udp.begin(port);
+	pinMode(pin_led, OUTPUT);
 }
 
 void loop() {
-	//reset watchdog timer
-	digitalWrite(pin_watch, HIGH);
-	delay(20);
-	digitalWrite(pin_watch, LOW);
+	if (millis() - lastTransmissionTime >= transmissionInterval)
+	{
+		//reset watchdog timer
+		digitalWrite(pin_watch, HIGH);
+		delay(20);
+		digitalWrite(pin_watch, LOW);
 	
-	//get sensor data
-	unsigned int pingTime = pinger.ping(); //in microseconds
-	float h = dht.readHumidity();
-	float t = dht.readTemperature();
+		//get sensor data
+		unsigned int pingTime = pinger.ping(); //in microseconds
+		float h = dht.readHumidity();
+		float t = dht.readTemperature();
 	
-	//check for a bad sensor read, set to 0 and handle server-side
-	//we know that relative humidity & temp are overwhelmingly unlikely to ever = 0
-	if (isnan(h)) {h = 0;}
-	if (isnan(t)) {t = 0;}
+		//check for a bad sensor read, set to 0 and handle server-side
+		//we know that relative humidity & temp are overwhelmingly unlikely to ever = 0
+		if (isnan(h)) {h = 0;}
+		if (isnan(t)) {t = 0;}
+		
+		//prepare packet data using json
+		StaticJsonBuffer<100> jsonBuffer;
+		JsonObject& data = jsonBuffer.createObject();
+		data["1"] = sensorType;
+		data["2"] = sensorId;
+		data["3"] = pingTime;
+		data["4"].set(h, 4);
+		data["5"].set(t, 4);
 	
-	//prepare packet data using json
-	StaticJsonBuffer<100> jsonBuffer;
-	JsonObject& data = jsonBuffer.createObject();
-	data["1"] = sensorType;
-	data["2"] = sensorId;
-	data["3"] = pingTime;
-	data["4"].set(h, 4);
-	data["5"].set(t, 4);
-
-	//send the packet
-	Udp.beginPacket(targetIp, port);
-	data.printTo(Udp);
-	Udp.println();
-	Udp.endPacket();
+		//send the packet
+		Udp.beginPacket(targetIp, port);
+		Udp.write(data);
+		Udp.endPacket();
+		
+		//cache transmission time and turn on indicator LED
+		lastTransmissionTime = millis();
+		ledState = HIGH;
+		digitalWrite(pin_led, ledState);
+	}
 	
-	//wait before next transmission
-	delay(waitTime);
+	if (ledState == HIGH || millis() - lastTransmissionTime >= ledInterval)
+	{
+		ledState = LOW;
+		digitalWrite(pin_led, ledState);
+	}
 }
